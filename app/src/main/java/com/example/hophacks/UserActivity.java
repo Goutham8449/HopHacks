cfb33563683458545c7d4c7b7b6e862ff598f347
Yuanxin Wang <y2587wang@uwaterloo.ca>
2020-09-12 14:25:52 -0400

    add google map

--- app/src/main/java/com/example/hophacks/UserActivity.java
+++ app/src/main/java/com/example/hophacks/UserActivity.java
@@ -1,28 +1,56 @@
 package com.example.hophacks;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends FragmentActivity implements OnMapReadyCallback {
     Button btnLogOut;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private Location mLastLocation;
+    private LocationManager locationManager;
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 200;
+    private static final String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION};
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS,
+                    REQUEST_CODE_LOCATION_PERMISSION);
+        }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mapFragment.getMapAsync(this);
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 FirebaseAuth.getInstance().signOut();
                 Intent I = new Intent(UserActivity.this, ActivityLogin.class);
                 startActivity(I);
@@ -30,5 +58,29 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS,
+                    REQUEST_CODE_LOCATION_PERMISSION);
+        }
+        if (mLastLocation == null) {
+
+            mLastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        }
+        if (mLastLocation == null) {
+            mLastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        // Add a marker in Sydney and move the camera
+        LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Your current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
     }
 }
